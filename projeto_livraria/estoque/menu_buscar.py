248c88e1e5626c7211f26ce46952eacc8435ab2e
augustomiguel <augusto.miguel@academico.ufpb.br>
2024-05-01 22:42:27 -0300

    .

--- projeto_livraria/estoque/menu_buscar.py
+++ projeto_livraria/estoque/menu_buscar.py
@@ -3,60 +3,88 @@ form .models import Livros
 class MenuBuscar:
     
     def exibir_menu(self, tipo );
-
-        print( "           Consulta de livros")
-        opcao = input("""
-Digite 0 para pesquisar por nome\n
-Digite 1 para pesquisar por faixa de preço\n
-Digite 2 para pesquisar por categorias\n>
-Digite 3 para pesquisar por vindos de Mari\n
-""")
-        if opcao == 0:
-            nome = input( "Digite o nome do livro")
-            livro = consultarNome(nome)
-            return livro
-        elif opcao == 1:
-            maior = input("Digite o maior preço\n>")
-            menor = input("Digite o menor preço\n>")
-            livro = consultarPreco
-            return livro
-            
-        elif opcao == 2:
-            categoria = input("Digite a Categoria\n>") 
-            consultarCategoria(categoria)
-        elif opcao == 3:
-            consultarMari()
-        else:
+        while true:
+            print( "           Consulta de livros")
+            opcao = input("""
+    Digite 0 para pesquisar por nome\n
+    Digite 1 para pesquisar por faixa de preço\n
+    Digite 2 para pesquisar por categorias\n>
+    Digite 3 para pesquisar por vindos de Mari\n
+    """)
+            if opcao == 0:
+                nome = input( "Digite o nome do livro")
+                livro = consultarNome(nome)
+                return livro
+            elif opcao == 1:
+                maior = input("Digite o maior preço\n>")
+                menor = input("Digite o menor preço\n>")
+                livro = consultarPreco
+                return livro
+                
+            elif opcao == 2:
+                categoria = input("Digite a Categoria\n>") 
+                livros = consultarCategoria(categoria)
+                return livros
+            elif opcao == 3:
+                livros = consultarMari()
+                return livros
+            else:
+                print("opção invalida!\n")
     
     
     def consultarNome(self ,nome):
         livro = Livro.get(categoria==nome)
-        if tipo == 0:
-            return livro
-        elif tipo == 1:
-            filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
-            if filtrar == 1
-                if livro.quantidade_em_estoque =<5: 
+        if livro.nome == nome:
+            if tipo == 0:
+                return livro
+            elif tipo == 1:
+                filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>")
+                if filtrar == 1
+                    if livro.quantidade_em_estoque =<5: 
+                        return livro
+                else:
                     return livro
             else:
-                return livro
+                print(livro)
+                return 
         else:
-            print(livro)
+            print("LIvro não encontrado")
+            return 0
             
     def consultarPreco(self, maior , menor):
+        if tipo == 1:
+            filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
         livros=none
         preco = menor
         while maior !=menor
             livros = Livro.get(preco)
+            if tipo == 0:
+                livros.append(livro)
+            elif tipo == 1:
+                if filtrar == 1
+                    if livro.quantidade_em_estoque =< 5: 
+                        livros.append(livro)
+                else:
+                    livros.append(livro)
+            else:
+                livros.append(livro)
             preco += 0.1
+            
+        if livros == none:
+            print("não temos livros nessa faixa de valores\n")
+            return
+        if tipo > 1:
+            return 
+        return livros
     
     def consultarCategoria(self,categoria):
         livro = Livro.get(categoria==categoria)
+        if tipo == 1:
+            filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
         if livro.categoria==categoria:
             if tipo == 0:
                 return livro
             elif tipo == 1:
-                filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
                 if filtrar == 1
                     if livro.quantidade_em_estoque =< 5: 
                         return livro
@@ -65,15 +93,19 @@ Digite 3 para pesquisar por vindos de Mari\n
             else:
                 livro = Livro.get(titulo==nome)
                 print(livro)
+                return
         else:
             print("Não temos livros dessa categoria")
+            
     def ConsultarMari(self):
         livro = Livro.get(origem==local)
+        if tipo == 1:
+            filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
+            
         if livro.origem==local:
             if tipo == 0:
                 return livro
             elif tipo == 1:
-                filtrar=input("Digte 1 para filtar por quantidade menor que 5\nDitite 0 para não filtrar\n>)
                 if filtrar == 1
                     if livro.quantidade_em_estoque =< 5: 
                         return livro
@@ -82,7 +114,7 @@ Digite 3 para pesquisar por vindos de Mari\n
             else:
                 livro = Livro.get(titulo==nome)
                 print(livro)
+                return
         else:
             print("Não temos livros de Mari")
 
-    
